e9082a845eda307e627e1437372fa9bc8c128e31
senaoApp <jimmy.huang@senao.com.tw>
2026-03-30 13:36:22 +0800

    Update iOS platform requirement from v12 to v13

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "LLCycleScrollView",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v13),
     ],
     products: [
         .library(
