680423851cbab3135a2bbb9b82f66ac3ea166768
senaoApp <jimmy.huang@senao.com.tw>
2026-03-30 11:42:28 +0800

    Initialize Package.swift for LLCycleScrollView

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "LLCycleScrollView",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "LLCycleScrollView",
+            targets: ["LLCycleScrollView"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/onevcat/Kingfisher.git",
+            from: "7.0.0"
+        ),
+    ],
+    targets: [
+        .target(
+            name: "LLCycleScrollView",
+            dependencies: [
+                .product(name: "Kingfisher", package: "Kingfisher"),
+            ],
+            path: "Lib/LLCycleScrollView",
+            resources: [
+                .process("LLCycleScrollView.bundle"),
+            ]
+        ),
+    ]
+)
